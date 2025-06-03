11462741410579d94eb06bc777e65028ce416806
gaurab_017 <gaurabghimire1234@gmail.com>
2025-06-03 12:56:47 +0545

    Handled Deleted request

--- app.js
+++ app.js
@@ -61,6 +61,18 @@ app.patch('/api/v1/tours/:id',(req,res)=>{
     }
   })
 })
+app.delete('/api/v1/tours/:id',(req,res)=>{
+  if(req.params.id * 1> tours.length){
+   res.status(404).json({
+      status:"fail",
+      message:"Invalid ID"
+    }) 
+  }
+  res.status(204).json({
+    status: 'sucess',
+    data:null
+  })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
