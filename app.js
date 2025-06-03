9cc6d47d6c6fcd5c43206916163876f718ab3303
gaurab_017 <gaurabghimire1234@gmail.com>
2025-06-03 11:39:37 +0545

    handled push request

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req,res)=>{
+    // console.log(req.body);
+    const newId = tours[tours.length-1].id+1;
+    const newTour = Object.assign({id:newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours, null,2),(err)=>{
+      res.status(200).json({
+        status : 'sucess',
+        data: {
+        tours :newTour
+        }
+      })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
