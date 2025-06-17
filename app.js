5c162532c63eef135d5f259fa2dfb467cd1649b7
gaurab_017 <gaurabghimire1234@gmail.com>
2025-06-17 13:46:59 +0545

    Implemented Global Error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const router = require('./routes/tourRoutes');
+const { Error } = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
@@ -27,12 +28,23 @@ app.use('/api/v1/tours', router);
 // Catch-all route for unmatched routes
 // '/*' is depreciated in express 5
 //So use /*any_name 
-app.all('/*routepath', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+app.all('/*routepath', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail'
+  err.statusCode =404;
+  next(err);
 });
-
+app.use((err,req,res,next)=>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+})
 
 module.exports = app;
